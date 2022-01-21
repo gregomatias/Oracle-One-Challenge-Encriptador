aacd588b8109fa379cc18b54a49c19002b69228a
vikingoarg <matiasgrego@gmail.com>
2022-01-21 20:30:59 -0300

    Primer subida

---
+++ js/app.js
@@ -0,0 +1,103 @@
+var botonEncriptar = document.querySelector("#btn-encriptar");
+var botonDesencriptar = document.querySelector("#btn-desencriptar");
+var botonCopiar = document.querySelector("#btn-copiar");
+var textAreaEncriptador = document.querySelector("#text-encriptador");
+var textAreaDesencriptador = document.querySelector("#text-desencriptador");
+var textAreaResultado = document.querySelector("#text-resultado");
+
+
+textAreaEncriptador.addEventListener("input", function () {
+
+    textAreaEncriptador.value = depuraTexto(textAreaEncriptador.value);
+});
+
+textAreaDesencriptador.addEventListener("input", function () {
+
+    textAreaDesencriptador.value = depuraTexto(textAreaDesencriptador.value);
+});
+
+botonEncriptar.addEventListener("click", function () {
+
+    console.log("entro");
+    textAreaResultado.value = encripta(textAreaEncriptador.value);
+    textAreaEncriptador.value = "";
+
+});
+
+botonDesencriptar.addEventListener("click", function () {
+
+
+    textAreaResultado.value = desEncripta(textAreaDesencriptador.value);
+    textAreaDesencriptador.value = "";
+
+});
+
+
+botonCopiar.addEventListener("click", function () {
+
+    copiaTexto();
+
+});
+
+//reemplaza mayusculas por minusculas, acentos por sin acento y caracteres extaños por vacio;
+function depuraTexto(textoIngresado) {
+console.log(textoIngresado);
+
+    var textoReemplazado = textoIngresado.replace(/á/g, "a");
+    textoReemplazado = textoReemplazado.replace(/é/g, "e");
+    textoReemplazado = textoReemplazado.replace(/í/g, "i");
+    textoReemplazado = textoReemplazado.replace(/ó/g, "o");
+    textoReemplazado = textoReemplazado.replace(/ú/g, "u");
+    textoReemplazado = textoReemplazado.replace(/[^A-Z-a-z]/g, "").toLowerCase();
+
+
+    return textoReemplazado;
+
+}
+
+function encripta(texto) {
+
+    var textoEncriptado = "";
+
+    textoEncriptado = texto.replace(/e/g, "enter");
+    textoEncriptado = textoEncriptado.replace(/i/g, "imes");
+    textoEncriptado = textoEncriptado.replace(/a/g, "ai");
+    textoEncriptado = textoEncriptado.replace(/o/g, "ober");
+    textoEncriptado = textoEncriptado.replace(/u/g, "ufat");
+
+    return textoEncriptado;
+
+
+}
+
+
+function desEncripta(texto) {
+
+    var textoEncriptado = "";
+
+    textoEncriptado = texto.replace(/enter/g, "e");
+    textoEncriptado = textoEncriptado.replace(/imes/g, "i");
+    textoEncriptado = textoEncriptado.replace(/ai/g, "a");
+    textoEncriptado = textoEncriptado.replace(/ober/g, "o");
+    textoEncriptado = textoEncriptado.replace(/ufat/g, "u");
+
+    return textoEncriptado;
+
+
+}
+
+
+function copiaTexto() {
+    var copyText = document.getElementById("text-resultado");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+}
+
